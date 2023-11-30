bd91347b81047242b1e571e9cb74329d68ea25e7
Loris Dié <lorisdie@gmail.com>
2023-11-30 13:45:00 +0100

    adding websockets + connexion/start popup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Popup from "./components/Popup";
 
 function App() {
+  const [isPopupOpen, setPopupOpen] = useState(true);
+  const [messageFromServer, setMessageFromServer] = useState("");
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:3000"); // Remplacez par l'URL de votre serveur WebSocket
+
+    ws.onopen = () => {
+      console.log("Connected to server");
+      ws.send("Hello from client"); // Envoyer un message au serveur
+    };
+
+    ws.onmessage = (event) => {
+      console.log("Received from server:", event.data);
+      setMessageFromServer(event.data);
+      // Gérez les données reçues du serveur, par exemple, déclenchez l'ouverture de la popup
+      setPopupOpen(true);
+    };
+
+    ws.onclose = () => {
+      console.log("Disconnected from server");
+    };
+
+    return () => {
+      ws.close(); // Fermez la connexion WebSocket lorsque le composant est démonté
+    };
+  }, []); // Effectuée uniquement lors du montage du composant
+
+  const handleClosePopup = () => {
+    setPopupOpen(false);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isPopupOpen && <Popup onClose={handleClosePopup} />}
       </header>
     </div>
   );
---
+++ src/components/Popup.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Popup = ({ onClose }) => {
+  return (
+    <div className=" fixed top-0 left-0 w-full h-full bg-red-200 flex justify-center items-center">
+      <div className="p-5 rounded-md">
+        <p>Contenu de la popup</p>
+        <button
+          onClick={onClose}
+          className="rounded-lg px-4 py-2 border-grey-500 border-2 hover:bg-red-500 hover:border-red-500 hover:cursor-pointer  "
+        >
+          {" "}
+          Start game
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
